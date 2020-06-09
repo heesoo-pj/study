6a2174251bca557da741263309dc83e139e207cb
leeheesoo <hsoo@mz.co.kr>
2020-06-09 14:26:23 +0900

    Revert "기존파일 추가"
    This reverts commit 6043f66227180200bb7da651499728100faa12bd [formerly eb68e42280540ab269df8491035cd03952209adb].
    
    
    Former-commit-id: 6c3b11a9b9c2f41288bb1cf5681e6cee11873f17

--- vue/nuxt/README.md
+++
@@ -1,2 +0,0 @@
-# nuxt-template
-Nuxt.js 기본 프로젝트 입니다.
--- vue/nuxt/nuxt-template-master/store/example.js
+++
@@ -1,4 +0,0 @@
-export const state = () => ({
-  eventName: 'example',
-  imageUrlRoot: `https://hongyi.s3.amazonaws.com/static/new-semester/images`
-})
