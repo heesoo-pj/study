7af14787cf43042a11b74412ca8941e1396820c3
leeheesoo <hsoo@mz.co.kr>
2020-08-10 14:55:52 +0900

    인터렉션 예제 추가

--- ex/js/index.js
+++ ex/js/index.js
@@ -1,20 +1,4 @@
-// index.js
-
 var express = require('express');
 var app = express();
 
-app.set('view engine','ejs'); // 1
-app.use(express.static(__dirname + '/public'));
-
-app.get('/hello', function(req,res){ // 2
-  res.render('hello', {name:req.query.nameQuery});
-});
-
-app.get('/hello/:nameParam', function(req,res){ // 3
-  res.render('hello', {name:req.params.nameParam});
-});
-
-var port = 3000;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
-});
+app.set('view engine', 'ejs'); // view engine으로 ejs를 사용하겠다는 의미
